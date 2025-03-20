f42b1a719d020d0da9c6fd8685a7945e62d7f019
s240 <2401707410@qq.com>
2025-03-20 18:22:59 +0800

    完成中间件casbin的代码框架，权限规则还未定义，角色判断也未填写

--- app/gateway/biz/dal/mysql/init.go
+++ app/gateway/biz/dal/mysql/init.go
@@ -1,9 +1,11 @@
 package mysql
 
 import (
-	"github.com/wuyuesong/douyinmall/app/gateway/conf"
+	"fmt"
+	"os"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	
 )
 
 var (
@@ -12,7 +14,8 @@ var (
 )
 
 func Init() {
-	DB, err = gorm.Open(mysql.Open(conf.GetConf().MySQL.DSN),
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:3306)/gateway?charset=utf8mb4&parseTime=True&loc=Local", os.Getenv("MYSQL_USER"), os.Getenv("MYSQL_PASSWORD"), os.Getenv("MYSQL_HOST"))
+	DB, err = gorm.Open(mysql.Open(dsn),
 		&gorm.Config{
 			PrepareStmt:            true,
 			SkipDefaultTransaction: true,
@@ -21,4 +24,5 @@ func Init() {
 	if err != nil {
 		panic(err)
 	}
+	
 }
